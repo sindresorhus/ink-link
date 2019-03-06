a60cf0b772b0c8d690068547ca6d221aa97e2148
Vadim Demedes <vdemedes@gmail.com>
2019-03-05 22:32:56 -0800

    Use ink-testing-library

--- test.js
+++ test.js
@@ -1,34 +1,8 @@
 import {serial as test} from 'ava';
 import React from 'react';
-import {render, Color} from 'ink';
+import {Color} from 'ink';
+import {render} from 'ink-testing-library';
 import clearModule from 'clear-module';
-
-// Fake process.stdout
-class Stream {
-	constructor() {
-		this.output = '';
-		this.columns = 100;
-	}
-
-	write(str) {
-		this.output = str;
-	}
-
-	get() {
-		return this.output;
-	}
-}
-
-const renderToString = node => {
-	const stream = new Stream();
-
-	render(node, {
-		stdout: stream,
-		debug: true
-	});
-
-	return stream.get();
-};
 
 test.beforeEach(() => {
 	clearModule.all();
@@ -42,24 +16,24 @@ test('render', t => {
 	process.env.FORCE_HYPERLINK = 1;
 	const Link = require('.');
 
-	const actual = renderToString(
+	const {lastFrame} = render(
 		<Link url="https://sindresorhus.com">
 			My{' '}<Color green>Website</Color>
 		</Link>
 	);
-	console.log(actual);
-	t.snapshot(actual);
+	console.log(lastFrame());
+	t.snapshot(lastFrame());
 });
 
 test('render fallback', t => {
 	process.env.FORCE_HYPERLINK = 0;
 	const Link = require('.');
 
-	const actual = renderToString(
+	const {lastFrame} = render(
 		<Link url="https://sindresorhus.com">
 			My Website
 		</Link>
 	);
-	console.log(actual);
-	t.snapshot(actual);
+	console.log(lastFrame());
+	t.snapshot(lastFrame());
 });
